1c5d2ec355d9eb36eee5f21f9e68532472ade981
QuantiniumX <vaibhavvx331@gmail.com>
2024-01-10 15:11:23 +0530

    renamed some file for better understanding

---
+++ 29_reverse_a_num.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "string.h"
+
+int main() {
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  char str[5];
+  sprintf(str, "%d", num); // integer to string
+  for (int i = (strlen(str) - 1); i >= 0; i--) {
+    printf("%c", str[i]);
+  }
+}
